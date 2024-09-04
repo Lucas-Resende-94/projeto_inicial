651779476e9e31be0daa3c0471a1a9660153edb6
Lucas Resende <lucas.1994.resende@gmail.com>
2024-09-04 09:34:48 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,25 @@
+print('oi mundo')
+
+#Variaveis
+
+idade = 30
+nome = 'Lucas'
+
+#print
+
+print(idade)
+print(nome)
+
+nova_idade = idade + 5
+
+print(nova_idade)
+
+
+#listas
+
+lista = [1, 2, 3, 4, 5]
+
+nova_lista = lista.reverse
+
+print(lista)
+print(nova_lista)
